9ff3ef26fbcfcbc9d7010451f18a912608e2a47d
Israel Mendoza <mbi1204@hotmail.com>
2020-04-09 21:31:58 -0500

    obtiene coordenadas

--- app/src/main/java/com/sienrgitec/painanirep/actividades/Login.java
+++ app/src/main/java/com/sienrgitec/painanirep/actividades/Login.java
@@ -1,10 +1,12 @@
 package com.sienrgitec.painanirep.actividades;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
@@ -14,15 +16,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.sienrgitec.painanirep.R;
 import com.sienrgitec.painanirep.configuracion.Globales;
+import com.sienrgitec.painanirep.model.ctUsuario;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
     public Globales globales;
 
     private static RequestQueue mRequestQueue;
     private String url = globales.URL;
+
 
     Button btnEntrar;
     TextView txtRegistro;
@@ -56,9 +75,26 @@ public class Login extends AppCompatActivity {
 
     public void BuscarUsuario(){
         btnEntrar.setEnabled(false);
+
+        final String vcUsuLog = etNombreU.getText().toString();
         final String password = etPasword.getText().toString();
-        Log.i("passw", password);
-        //Globales.g_ctEmpleado = null;
+
+        if (etNombreU.getText().toString().isEmpty()) {
+            AlertDialog.Builder myBuild = new AlertDialog.Builder(Login.this);
+            myBuild.setMessage("No se capturo el nombre de usuario");
+            myBuild.setTitle(Html.fromHtml("<font color ='#FF0000'> ERROR </font>"));
+            myBuild.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    btnEntrar.setEnabled(true);
+
+                }
+            });
+            AlertDialog dialog = myBuild.create();
+            dialog.show();
+            return;
+        }
 
 
         if (etPasword.getText().toString().isEmpty()) {
@@ -66,26 +102,139 @@ public class Login extends AppCompatActivity {
             myBuild.setMessage("No se capturo el password del usuario");
             myBuild.setTitle(Html.fromHtml("<font color ='#FF0000'> ERROR </font>"));
             myBuild.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
+                    btnEntrar.setEnabled(true);
 
                 }
             });
-
             AlertDialog dialog = myBuild.create();
             dialog.show();
             return;
         }
 
+        getmRequestQueue();
+        String urlParams = String.format(url + "login?ipcUsuario=%1$s&ipcPassword=%2$s", vcUsuLog, password );
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, urlParams, null, new Response.Listener<JSONObject>() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONObject respuesta = response.getJSONObject("response");
+                            Log.i("respuesta--->", respuesta.toString());
+
+                            String Mensaje = respuesta.getString("opcError");
+                            Boolean Error = respuesta.getBoolean("oplError");
+                            JSONObject ds_ctUsuario = respuesta.getJSONObject("tt_ctUsuario");
+
+                            JSONArray tt_ctUsuario  = ds_ctUsuario.getJSONArray("tt_ctUsuario");
+
+                            globales.g_ctUsuarioList     = Arrays.asList(new Gson().fromJson(tt_ctUsuario.toString(), ctUsuario[].class));
+
+
+                            if (Error == true) {
+                                btnEntrar.setEnabled(true);
+                                MuestraMensaje("Error", Mensaje);
+                                return;
+
+                            } else {
+
+
+                            }
+                        } catch (JSONException e) {
+                            btnEntrar.setEnabled(true);
+                            AlertDialog.Builder myBuild = new AlertDialog.Builder(Login.this);
+                            myBuild.setMessage("Error en la conversión de Datos. Vuelva a Intentar. " + e);
+                            myBuild.setTitle(Html.fromHtml("<font color ='#FF0000'> ERROR CONVERSION </font>"));
+                            myBuild.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+
+                                }
+                            });
+                            AlertDialog dialog = myBuild.create();
+                            dialog.show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        btnEntrar.setEnabled(true);
+                        // TODO: Handle error
+                        Log.i("Error Respuesta", error.toString());
+                        AlertDialog.Builder myBuild = new AlertDialog.Builder(Login.this);
+                        myBuild.setMessage("No se pudo conectar con el servidor. Vuelva a Intentar. " + error.toString());
+                        myBuild.setTitle(Html.fromHtml("<font color ='#FF0000'> ERROR RESPUESTA </font>"));
+                        myBuild.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                        AlertDialog dialog = myBuild.create();
+                        dialog.show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("ipcUsuario", vcUsuLog);
+                params.put("ipcPassword", password);
+
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        // Access the RequestQueue through your singleton class.
+        mRequestQueue.add(jsonObjectRequest);
+
+
 
     }
+    public void getmRequestQueue(){
+        try{
+            if (mRequestQueue == null) {
+                mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+                //your code
+            }
+        }catch(Exception e){
+            Log.d("Volley",e.toString());
+        }
+    }
+
     public void NuevoUsuario(){
         startActivity(new Intent(Login.this, ValidaDir.class));
         finish();
     }
-    public void MuestraMensaje(){
+    public void MuestraMensaje(String vcTitulo,  String vcMensaje){
+        AlertDialog.Builder myBuild = new AlertDialog.Builder(Login.this);
+        myBuild.setMessage(vcMensaje);
+        myBuild.setTitle(Html.fromHtml("<font color ='#FF0000'>" + vcTitulo +"</font>"));
+        myBuild.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                return;
+
+            }
+        });
+        AlertDialog dialog = myBuild.create();
+        dialog.show();
+        return;
 
     }
 
--- app/src/main/java/com/sienrgitec/painanirep/configuracion/Globales.java
+++ app/src/main/java/com/sienrgitec/painanirep/configuracion/Globales.java
@@ -6,11 +6,14 @@ import com.sienrgitec.painanirep.model.ctPainani;
 import com.sienrgitec.painanirep.model.ctUsuario;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Globales {
 
     public static  String  URL = "http://192.168.1.102:8083/painal/rest/painalService/";
 
+    /*Catalogos*/
+    public static List<ctUsuario> g_ctUsuarioList  = null;
 
 
     public static ArrayList<ctDomicilio> ctDomicilioList = new ArrayList<>();
