92cf1b68b195d811a037e1134de94e9f65725132
AndrosSinergitec <51095324+AndrosSinergitec@users.noreply.github.com>
2020-04-09 21:41:44 -0500

    datos del contacto y datos usuario

--- app/src/main/java/com/sienrgitec/painanirep/actividades/CreaContacto.java
+++ app/src/main/java/com/sienrgitec/painanirep/actividades/CreaContacto.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.sienrgitec.painanirep.R;
 
 public class CreaContacto extends AppCompatActivity {
 
     Button btnCreaRegistro;
+    EditText etNombreUsu;
+    EditText etPwReg;
+    EditText etNombreC;
+    EditText etApellidosC;
+    EditText etObsC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
